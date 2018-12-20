5124d6717c75e7acca2400a5c93fe8c9e7d32885
Snow <511422713@qq.com>
2018-12-20 19:45:27 +0800

    Add: read data(without bitch)

--- py/util/data_generator.py
+++ py/util/data_generator.py
@@ -2,53 +2,64 @@ import os
 import csv
 import operator
 import numpy as np
-from py.util.byte_code_tool import generate_bytecode
 
-cpp_dir_path = '../../cpp_raw/add/'
 ll_dir_path = '../../cpp_bc/'
+x = 145
+y = 16
 
 
-def get_ll():
+def print_hex(my_byte):
+    string = [hex(int(i)) for i in my_byte]
+    print(" ".join(string))
+
+
+def get_ll(path):
     lls = []
-    generate_bytecode(cpp_dir_path)
-    for ll in sorted(os.listdir(ll_dir_path)):
-        with open(ll_dir_path + ll, 'rb') as f:
-            byte = f.read(1)
-            bytes = []
-            while byte:
+    for ll in sorted(os.listdir(ll_dir_path + path)):
+        with open(ll_dir_path + path + ll, 'rb') as f:
+            byte_array = []
+            n = 0
+            while n < x * y:
                 byte = f.read(1)
-                bytes.append(byte)
-            lls.append(bytes)
-
+                if byte:
+                    byte_array.append(byte)
+                else:
+                    byte_array.append(b'00')
+                n = n + 1
+            lls.append(byte_array)
     return lls
 
 
-def get_error():
+def get_all_ll():
+    return get_ll('add/') + get_ll('mul/')
+
+
+def get_error(path):
     error_list = []
-    for f in sorted(os.listdir('/root/tmp/dataset/')):
+    for f in sorted(os.listdir('../../dataset/' + path)):
         if f.split('.')[-1] == 'csv':
-            error_list.append(get_top5(f))
+            error_list.append(get_top5_input(path + f))
     return error_list
 
 
-def get_top5(csv_name):
-    reader = csv.reader(open('/root/tmp/dataset/' + csv_name), delimiter=',')
+def get_all_error():
+    return get_error('add/') + get_error('mul/')
+
+
+def get_top5_input(csv_name):
+    reader = csv.reader(open('../../dataset/' + csv_name), delimiter=',')
     data_list = dict(reader)
     for k, v in data_list.items():
         data_list[k] = float(v)
     sorted_list = sorted(data_list.items(), key=operator.itemgetter(1), reverse=True)
-    return sorted_list[:5]
+    # only use the input
+    return [input[0] for input in sorted_list[:5]]
 
 
 def read_data():
-    lls = get_ll()
-    errors = get_error()
-    print('Length of ll: ' + str(len(lls)))
+    lls = get_all_ll()
+    errors = get_all_error()
     lls = np.array(lls)
     errors = np.array(errors)
-    print("shape of lls: {}\tshape of errors: {}".format(lls.shape, errors.shape))
+    print('shape of lls: {}\tshape of errors: {}'.format(lls.shape, errors.shape))
     return lls, errors
-
-
-if __name__ == '__main__':
-    read_data()
